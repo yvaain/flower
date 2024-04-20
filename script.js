3804f7ddb63ce2d14846af4327fbb8b43b4cacb4
yvaain <167555678+yvaain@users.noreply.github.com>
2024-04-20 19:44:24 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
 
-onload = () => {
-  const c = setTimeout(() => {
-    document.body.classList.remove("not-loaded");
-    clearTimeout(c);
-  }, 1000);
+onload = () =>{
+        document.body.classList.remove("container");
 };
